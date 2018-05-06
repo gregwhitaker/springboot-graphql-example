c48435ffc78e41e451c89a87e67aa66d95172872
gregwhitaker <gwhitake@gmail.com>
2018-05-06 16:38:53 -0700

    Added schema

--- src/main/java/springboot/graphql/example/Application.java
+++ src/main/java/springboot/graphql/example/Application.java
@@ -1,8 +1,12 @@
 package springboot.graphql.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
